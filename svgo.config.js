6594bdf2e4fdd68f0d4274a27663a5b577c09c21
Yannick <yannick.franssen@outlook.com>
2022-10-20 15:58:41 +0200

    Update SVGO to support icons

--- svgo.config.js
+++ svgo.config.js
@@ -2,37 +2,60 @@
 
 module.exports = {
   multipass: true,
+  js2svg: {
+    eol: "lf"
+  },
   plugins: [
     {
       name: "preset-default",
       params: {
         overrides: {
-          convertPathData: {
-            floatPrecision: 2,
-          },
           removeUnknownsAndDefaults: {
-            keepRoleAttr: true,
+            keepRoleAttr: true
           },
-        },
-      },
+          removeViewBox: false
+        }
+      }
     },
     "cleanupListOfValues",
     "sortAttrs",
     {
       name: "removeAttrs",
       params: {
-        attrs: ["clip-rule", "data-name", "fill", "height", "width"],
-      },
+        attrs: ["clip-rule", "data-name", "fill"]
+      }
     },
     {
-      name: "addAttributesToSVGElement",
+      name: "explicitAttrs",
+      type: "visitor",
       params: {
-        attributes: [
-          {
-            fill: "currentColor",
-          },
-        ],
+        attributes: {
+          xmlns: "http://www.w3.org/2000/svg",
+          width: "16",
+          height: "16",
+          fill: "currentColor",
+          class: "vi",
+          viewBox: "0 0 16 16"
+        }
       },
-    },
-  ],
+      fn(_root, params) {
+        if (!params.attributes) {
+          return null;
+        }
+
+        return {
+          element: {
+            enter(node, parentNode) {
+              if (node.name === "svg" && parentNode.type === "root") {
+                node.attributes = {};
+                for (const [key, value] of Object.entries(params.attributes)) {
+                  node.attributes[key] = value;
+                }
+              }
+            }
+          }
+        };
+      }
+    }
+  ]
 };
