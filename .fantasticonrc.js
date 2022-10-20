e012a36b5e5293a2effff181115e171b7f1497e2
Yannick <yannick.franssen@outlook.com>
2022-10-20 10:28:11 +0200

    Disable normalization

--- .fantasticonrc.js
+++ .fantasticonrc.js
@@ -1,30 +1,29 @@
-const codepoints = require('./dist/fonts/valkyrie.json')
+const codepoints = require("./dist/fonts/valkyrie.json");
 
 module.exports = {
-  inputDir: './icons',
-  outputDir: './dist',
-  fontTypes: ['ttf', 'woff', 'woff2', 'eot'],
-  assetTypes: ['css', 'scss', 'html', 'json'],
-  fontsUrl: '.',
-  name: 'Valkyrie',
-  prefix: 'vi',
-  selector: '.vi',
+  inputDir: "./icons",
+  outputDir: "./dist",
+  fontTypes: ["ttf", "woff", "woff2", "eot"],
+  assetTypes: ["css", "scss", "html", "json"],
+  fontsUrl: ".",
+  name: "Valkyrie",
+  prefix: "vi",
+  selector: ".vi",
   codepoints,
-  normalize: true,
   descent: 26,
   templates: {
-    html: './templates/html.hbs',
-    css: './templates/css.hbs',
-    scss: './templates/css.hbs'
+    html: "./templates/html.hbs",
+    css: "./templates/css.hbs",
+    scss: "./templates/css.hbs",
   },
   pathOptions: {
-    eot: './dist/fonts/Valkyrie.eot',
-    ttf: './dist/fonts/Valkyrie.ttf',
-    woff: './dist/fonts/Valkyrie.woff',
-    woff2: './dist/fonts/Valkyrie.woff2',
-    css: './dist/fonts/Valkyrie.css',
-    scss: './dist/fonts/Valkyrie.scss',
-    html: './docs/index.html',
-    json: './dist/fonts/valkyrie.json',
-  }
+    eot: "./dist/fonts/Valkyrie.eot",
+    ttf: "./dist/fonts/Valkyrie.ttf",
+    woff: "./dist/fonts/Valkyrie.woff",
+    woff2: "./dist/fonts/Valkyrie.woff2",
+    css: "./dist/fonts/Valkyrie.css",
+    scss: "./dist/fonts/Valkyrie.scss",
+    html: "./docs/index.html",
+    json: "./dist/fonts/valkyrie.json",
+  },
 };
