566ff76bbba9d83803de39367e762ce8a095d4d2
Yannick <yannick.franssen@outlook.com>
2023-08-10 16:57:48 +0200

    Refresh home and details page

--- site/pages/icons/[slug].js
+++ site/pages/icons/[slug].js
@@ -1,17 +1,17 @@
 import React from "react";
 import matter from "gray-matter";
-import { Container } from "@mui/system";
 import {
   Box,
   Button,
   Card,
   Chip,
-  Divider,
-  Grid,
+  Container,
   IconButton,
   Input,
+  Link,
   Stack,
-  Typography
+  Typography,
+  useColorScheme
 } from "@mui/joy";
 import Head from "next/head";
 import ValkyrieIcon from "@sippy-platform/valkyrie";
@@ -19,6 +19,7 @@ import * as Icons from "@sippy-platform/valkyrie";
 import NextLink from "next/link";
 
 function PostTemplate({ data, slug }) {
+  const { mode, setMode } = useColorScheme();
   const frontmatter = data;
 
   const icon = `vi${slug
@@ -35,106 +36,138 @@ function PostTemplate({ data, slug }) {
         <meta name="description" content="The Valkyrie Icon set." />
         <link rel="icon" href="/favicon.svg" />
       </Head>
-      <Stack
-        direction="row"
-        spacing={1}
-        justifyContent="flex-start"
-        alignItems="center"
-        sx={{ py: 2 }}
-      >
-        <NextLink href="/">
-          <IconButton variant="outlined">
-            <ValkyrieIcon icon={Icons.viArrowLeft} />
+
+      <Stack spacing={2} sx={{ py: 2 }}>
+        <Stack
+          direction="row"
+          spacing={1}
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Typography
+            level="h1"
+            sx={{
+              display: "inline",
+              backgroundImage: "linear-gradient(135deg, #0061c9, #c455f7)",
+              WebkitBackgroundClip: "text",
+              WebkitTextFillColor: "transparent"
+            }}
+          >
+            Valkyrie
+          </Typography>
+          <IconButton
+            variant="outlined"
+            size="sm"
+            onClick={() =>
+              setMode(
+                mode === "light" ? "dark" : mode === "dark" ? "system" : "light"
+              )
+            }
+          >
+            <ValkyrieIcon
+              icon={
+                mode === "light"
+                  ? Icons.viSun
+                  : mode === "dark"
+                  ? Icons.viMoon
+                  : Icons.viCircleHalfInner
+              }
+            />
           </IconButton>
-        </NextLink>
-        <Typography
-          level="h1"
-          sx={{
-            display: "inline",
-            backgroundImage: "linear-gradient(135deg, #0061c9, #c455f7)",
-            WebkitBackgroundClip: "text",
-            WebkitTextFillColor: "transparent"
-          }}
+        </Stack>
+        <Stack
+          direction="row"
+          spacing={1}
+          justifyContent="space-between"
+          alignItems="center"
         >
-          Valkyrie
-        </Typography>
-      </Stack>
-      <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
-        <Typography level="h1" fontSize="xl3" fontWeight="xl">
-          {frontmatter.title}
-        </Typography>
-      </Box>
-      {(frontmatter.categories || frontmatter.tags) && (
-        <Box sx={{ display: "flex", gap: 0.5, alignItems: "center", mt: 2 }}>
-          {frontmatter.categories?.map(cat => (
-            <Chip variant="solid" color="primary" key={cat}>
-              {cat}
-            </Chip>
-          ))}
-          {frontmatter.tags?.map(tag => (
-            <Chip key={tag}>{tag}</Chip>
-          ))}
-        </Box>
-      )}
-      <Stack direction={{ xs: "column", md: "row" }} spacing={2} sx={{ pt: 2 }}>
-        <Card
-          variant="outlined"
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            borderRadius: 8,
-            backgroundColor: "#fdfdfd",
-            backgroundImage:
-              "radial-gradient(circle, #ddd 1px, rgba(0, 0, 0, 0) 1px)",
-            backgroundSize: "1rem 1rem",
-            backgroundPosition: "calc(.5rem - 1px) calc(.5rem - 1px)",
-            fontSize: "10rem",
-            width: "20rem",
-            height: "20rem"
-          }}
-        >
-          <ValkyrieIcon icon={Icons[icon]} />
-        </Card>
-        <Card variant="outlined" sx={{ flexGrow: 1 }}>
-          <Typography level="h3" sx={{ mb: 3 }}>
-            <ValkyrieIcon icon={Icons[icon]} /> Heading icon
-          </Typography>
-          <Typography level="body-md" sx={{ mb: 3 }}>
-            <ValkyrieIcon icon={Icons[icon]} /> Inline icon
-          </Typography>
-          <Box sx={{ mb: 3, display: "flex", gap: 1 }}>
-            <Button startDecorator={<ValkyrieIcon icon={Icons[icon]} />}>
-              Button icon
-            </Button>
-            <IconButton color="primary" variant="soft">
-              <ValkyrieIcon icon={Icons[icon]} />
-            </IconButton>
+          <Stack direction="row" alignContent="center" spacing={1}>
+            <Typography level="h2" fontSize="xl2" fontWeight="md">
+              <NextLink href="/">
+                <Link color="neutral">Icons</Link>
+              </NextLink>{" "}
+              <Typography fontWeight="xl">{frontmatter.title}</Typography>
+            </Typography>
+          </Stack>
+        </Stack>
+
+        {(frontmatter.categories || frontmatter.tags) && (
+          <Box sx={{ display: "flex", gap: 0.5, alignItems: "center" }}>
+            {frontmatter.categories?.map(cat => (
+              <Chip variant="solid" color="primary" size="sm" key={cat}>
+                {cat}
+              </Chip>
+            ))}
+            {frontmatter.tags?.map(tag => (
+              <Chip key={tag} variant="outlined" size="sm">
+                {tag}
+              </Chip>
+            ))}
           </Box>
-          <Input
-            startDecorator={<ValkyrieIcon icon={Icons[icon]} />}
-            placeholder={frontmatter.title}
-          />
-        </Card>
+        )}
+
+        <Stack direction={{ xs: "column", md: "row" }} spacing={2}>
+          <Card
+            variant="outlined"
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: 8,
+              backgroundImage:
+                "radial-gradient(circle, var(--joy-palette-neutral-500) 1px, rgba(0, 0, 0, 0) 1px)",
+              backgroundSize: "1rem 1rem",
+              backgroundPosition: "calc(.5rem - 1px) calc(.5rem - 1px)",
+              fontSize: "10rem",
+              width: "20rem",
+              height: "20rem"
+            }}
+          >
+            <ValkyrieIcon icon={Icons[icon]} />
+          </Card>
+          <Card variant="outlined" sx={{ flexGrow: 1 }}>
+            <Typography level="h3" sx={{ mb: 3 }}>
+              <ValkyrieIcon icon={Icons[icon]} /> Heading icon
+            </Typography>
+            <Typography level="body-md" sx={{ mb: 3 }}>
+              <ValkyrieIcon icon={Icons[icon]} /> Inline icon
+            </Typography>
+            <Box sx={{ mb: 3, display: "flex", gap: 1 }}>
+              <Button startDecorator={<ValkyrieIcon icon={Icons[icon]} />}>
+                Button icon
+              </Button>
+              <IconButton color="primary" variant="soft">
+                <ValkyrieIcon icon={Icons[icon]} />
+              </IconButton>
+            </Box>
+            <Input
+              startDecorator={<ValkyrieIcon icon={Icons[icon]} />}
+              placeholder={frontmatter.title}
+            />
+          </Card>
+        </Stack>
+        <Stack direction={{ xs: "column", md: "row" }} spacing={2}>
+          <Box sx={{ flexGrow: 1 }}>
+            <Typography level="h3" sx={{ mb: 2 }}>
+              HTML
+            </Typography>
+            <Card variant="outlined" sx={{ py: 2 }}>
+              <code>{`<i class="ai ai-${slug.replaceAll(
+                " ",
+                "-"
+              )}"></i>`}</code>
+            </Card>
+          </Box>
+          <Box sx={{ flexGrow: 1 }}>
+            <Typography level="h3" sx={{ mb: 2 }}>
+              React
+            </Typography>
+            <Card variant="outlined" sx={{ py: 2 }}>
+              <code>{`<ValkyrieIcon icon={${icon}} />`}</code>
+            </Card>
+          </Box>
+        </Stack>
       </Stack>
-      <Grid container spacing={2} sx={{ mt: 3 }}>
-        <Grid item xs={12} sm={6}>
-          <Typography level="h3" sx={{ mb: 2 }}>
-            HTML
-          </Typography>
-          <Card variant="outlined" sx={{ py: 2 }}>
-            <code>{`<i class="vi vi-${slug.replaceAll(" ", "-")}"></i>`}</code>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Typography level="h3" sx={{ mb: 2 }}>
-            React
-          </Typography>
-          <Card variant="outlined" sx={{ py: 2 }}>
-            <code>{`<ValkyrieIcon icon={${icon}} />`}</code>
-          </Card>
-        </Grid>
-      </Grid>
     </Container>
   );
 }
--- site/pages/index.js
+++ site/pages/index.js
@@ -1,14 +1,13 @@
 import {
   Box,
   Container,
-  Divider,
-  Grid,
   Link,
   Input,
   Typography,
   Stack,
   Sheet,
-  IconButton
+  IconButton,
+  useColorScheme
 } from "@mui/joy";
 import Head from "next/head";
 import fs from "fs";
@@ -49,6 +48,8 @@ export async function getStaticProps() {
 }
 
 export default function Home({ icons }) {
+  const { mode, setMode } = useColorScheme();
+
   const [page, setPage] = useState(0);
 
   const { result, needle, setNeedle } = useSearch(icons, [
@@ -58,133 +59,165 @@ export default function Home({ icons }) {
   ]);
 
   return (
-    <Container>
+    <Container maxWidth="xl">
       <Head>
         <title>Valkyrie</title>
         <meta name="description" content="The Valkyrie Icon set." />
         <link rel="icon" href="/favicon.svg" />
       </Head>
 
-      <Stack direction="row" spacing={1} justifyContent="flex-start" alignItems="center" sx={{ py: 2 }}>
-        <Typography
-          level="h1"
+      <Stack spacing={2} sx={{ py: 2 }}>
+        <Stack
+          direction="row"
+          spacing={1}
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Typography
+            level="h1"
+            sx={{
+              display: "inline",
+              backgroundImage: "linear-gradient(135deg, #0061c9, #c455f7)",
+              WebkitBackgroundClip: "text",
+              WebkitTextFillColor: "transparent"
+            }}
+          >
+            Valkyrie
+          </Typography>
+          <IconButton
+            variant="outlined"
+            size="sm"
+            onClick={() =>
+              setMode(
+                mode === "light" ? "dark" : mode === "dark" ? "system" : "light"
+              )
+            }
+          >
+            <ValkyrieIcon
+              icon={
+                mode === "light"
+                  ? Icons.viSun
+                  : mode === "dark"
+                  ? Icons.viMoon
+                  : Icons.viCircleHalfInner
+              }
+            />
+          </IconButton>
+        </Stack>
+        <Stack
+          direction="row"
+          spacing={1}
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Stack direction="row" spacing={1} alignItems="baseline">
+            <Typography level="h2" sx={{ fontWeight: 700, fontSize: "xl2" }}>
+              {result.length} icons
+            </Typography>
+            <Typography color="neutral">
+              Page {page + 1} of {Math.ceil(result.length / 180)}
+            </Typography>
+          </Stack>
+
+          <Input
+            startDecorator={<ValkyrieIcon icon={Icons.viMagnifyingGlass} />}
+            placeholder="Search"
+            value={needle}
+            onChange={e => {
+              setNeedle(e.target.value);
+              setPage(0);
+            }}
+          />
+        </Stack>
+
+        <Box
           sx={{
-            display: 'inline',
-            backgroundImage: "linear-gradient(135deg, #0061c9, #c455f7)",
-            WebkitBackgroundClip: "text",
-            WebkitTextFillColor: "transparent"
+            display: "grid",
+            gridTemplateColumns:
+              "repeat(auto-fill, minmax(min(9.5rem, 100%), 1fr))",
+            gap: { xs: 1 }
           }}
         >
-          Valkyrie
-        </Typography>
-      </Stack>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          mb: 2
-        }}
-      >
-        <Typography level="h2" sx={{ fontWeight: 700, fontSize: "1.75rem" }}>
-          Icons
-        </Typography>
-
-        <Input
-          startDecorator={<ValkyrieIcon icon={Icons.viMagnifyingGlass} />}
-          placeholder="Search"
-          value={needle}
-          onChange={e => {
-            setNeedle(e.target.value);
-            setPage(0);
-          }}
-        />
-      </Box>
-      <Grid container spacing={2}>
-        {result.slice(page * 42, (page + 1) * 42).map(({ slug, viIcon }) => (
-          <Grid item xs={4} sm={3} md={2} key={slug}>
-            <Sheet
-              variant="outlined"
-              sx={{
-                gap: 0,
-                borderRadius: "sm",
-                "&:hover, &:focus-within": {
-                  backgroundColor:
-                    "rgba(var(--joy-palette-primary-mainChannel) / .0625)",
-                  "& > div > a > .MuiTypography-root": {
+          {result
+            .slice(page * 180, (page + 1) * 180)
+            .map(({ slug, viIcon }) => (
+              <Sheet
+                variant="outlined"
+                sx={{
+                  gap: 0,
+                  borderRadius: "sm",
+                  "&:hover, &:focus-within": {
                     backgroundColor:
-                      "rgba(var(--joy-palette-primary-mainChannel) / .125)"
+                      "rgba(var(--joy-palette-primary-mainChannel) / .0625)",
+                    "& > div > a > .MuiTypography-root": {
+                      backgroundColor:
+                        "rgba(var(--joy-palette-primary-mainChannel) / .125)"
+                    }
                   }
-                }
-              }}
-            >
-              <Stack
-                gap={3}
-                justifyContent="center"
-                alignItems="center"
-                sx={{ pt: 3, pb: 1 }}
+                }}
               >
-                {Icons?.[viIcon] && (
-                  <ValkyrieIcon icon={Icons[viIcon]} sx={{ fontSize: 32 }} />
-                )}
-                <Link
-                  overlay
-                  component={NextLink}
-                  href={`/icons/${slug}`}
-                  underline="none"
-                  color="neutral"
-                  sx={{
-                    maxWidth: "calc(100% - 32px)"
-                  }}
+                <Stack
+                  gap={3}
+                  justifyContent="center"
+                  alignItems="center"
+                  sx={{ pt: 3, pb: 1 }}
                 >
-                  <Typography
-                    noWrap
-                    level="body-sm"
+                  {Icons?.[viIcon] && (
+                    <ValkyrieIcon icon={Icons[viIcon]} sx={{ fontSize: 32 }} />
+                  )}
+                  <Link
+                    overlay
+                    component={NextLink}
+                    href={`/icons/${slug}`}
+                    underline="none"
+                    color="neutral"
                     sx={{
-                      px: 0.5,
-                      py: 0.25,
-                      borderRadius: "sm",
-                      fontFamily:
-                        "SFMono-Regular,Menlo,Monaco,Consolas,Liberation Mono,Courier New,monospace"
+                      maxWidth: "calc(100% - 32px)"
                     }}
                   >
-                    {viIcon}
-                  </Typography>
-                </Link>
-              </Stack>
-            </Sheet>
-          </Grid>
-        ))}
-        <Grid xs={12}>
-          <Stack
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-            gap={1}
-            sx={{ mb: 3 }}
+                    <Typography
+                      noWrap
+                      level="body-sm"
+                      sx={{
+                        px: 0.5,
+                        py: 0.25,
+                        borderRadius: "sm",
+                        fontFamily:
+                          "SFMono-Regular,Menlo,Monaco,Consolas,Liberation Mono,Courier New,monospace"
+                      }}
+                    >
+                      {viIcon}
+                    </Typography>
+                  </Link>
+                </Stack>
+              </Sheet>
+            ))}
+        </Box>
+
+        <Stack
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          gap={1}
+        >
+          <IconButton
+            variant="solid"
+            color="primary"
+            onClick={() => setPage(prev => prev - 1)}
+            disabled={page === 0}
           >
-            <IconButton
-              variant="solid"
-              color="primary"
-              onClick={() => setPage(prev => prev - 1)}
-              disabled={page === 0}
-            >
-              <ValkyrieIcon icon={Icons.viChevronLeft} />
-            </IconButton>
-            <Typography>
-              {page + 1}/{Math.ceil(result.length / 42)}
-            </Typography>
-            <IconButton
-              variant="solid"
-              color="primary"
-              onClick={() => setPage(prev => prev + 1)}
-              disabled={page === Math.ceil(result.length / 42) - 1}
-            >
-              <ValkyrieIcon icon={Icons.viChevronRight} />
-            </IconButton>
-          </Stack>
-        </Grid>
-      </Grid>
+            <ValkyrieIcon icon={Icons.viChevronLeft} />
+          </IconButton>
+          <IconButton
+            variant="solid"
+            color="primary"
+            onClick={() => setPage(prev => prev + 1)}
+            disabled={page === Math.ceil(result.length / 180) - 1}
+          >
+            <ValkyrieIcon icon={Icons.viChevronRight} />
+          </IconButton>
+        </Stack>
+      </Stack>
     </Container>
   );
 }
