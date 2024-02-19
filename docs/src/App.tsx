3f8fecbaf8bbd83c29e1f433b59dae843fc191c3
Yannick <yannick.franssen@outlook.com>
2024-02-19 13:45:25 +0100

    Fix App component

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,19 +1,15 @@
-import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 
 import { CssBaseline, CssVarsProvider } from '@mui/joy';
 
 import joyTheme from './design/joyTheme';
 import router from './Router';
-import store from './store';
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <CssVarsProvider theme={joyTheme} defaultMode="system">
-        <CssBaseline />
-        <RouterProvider router={router} />
-      </CssVarsProvider>
-    </Provider>
+    <CssVarsProvider theme={joyTheme} defaultMode="system">
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </CssVarsProvider>
   );
 }
