8312a8a8069461cd3adac249ba96f7e7b88a5371
Yannick <yannick.franssen@outlook.com>
2025-06-26 10:47:40 +0200

    Update dependencies, remove tailwind

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,5 +1,4 @@
 import * as path from 'path';
-import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
@@ -8,7 +7,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
     visualizer({
       title: 'Package size',
       filename: 'stats/index.html',
