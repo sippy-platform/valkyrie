6b0c27ede1550d5125f2b85fa133469334af6567
Yannick <yannick.franssen@outlook.com>
2025-10-23 15:48:28 +0200

    Update Teams icon, add calendar-arrow-to-day

--- docs/src/app/Docs/pages/Changelog.tsx
+++ docs/src/app/Docs/pages/Changelog.tsx
@@ -7,6 +7,7 @@ import Release from './_Release';
 export default function Changelog() {
   return (
     <Stack gap={5}>
+      <Release name="Valkyrie 1.0 beta 4" version="1.0.0-beta.4" date="October 2025" newIcons={['calendar-arrow-to-day']} updatedIcons={['teams']} />
       <Release
         name="Valkyrie 1.0 beta 3"
         version="1.0.0-beta.3"
