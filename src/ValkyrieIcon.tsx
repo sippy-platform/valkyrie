7fd2f399fe50cdf0873b788eeadd253de8043686
Yannick <yannick.franssen@outlook.com>
2025-12-09 12:29:44 +0100

    Minor adjustments to ValkyrieIcon component

--- src/ValkyrieIcon.tsx
+++ src/ValkyrieIcon.tsx
@@ -14,30 +14,30 @@ interface ValkyrieProps {
 }
 
 export default function ValkyrieIcon({
-  flip = undefined,
   icon,
+  className,
+  beat = undefined,
+  bounce = undefined,
+  fade = undefined,
+  flip = undefined,
   rotate = undefined,
   spin = undefined,
-  beat = undefined,
-  fade = undefined,
-  bounce = undefined,
-  className,
   ...props
 }: ValkyrieProps & ComponentPropsWithoutRef<"span">) {
   return (
     <span
       className={clsx(className, "vi-icon", {
-        ["vi-rotate-90"]: rotate === 90,
-        ["vi-rotate-180"]: rotate === 180,
-        ["vi-rotate-270"]: rotate === 270,
-        ["vi-flip-x"]: flip === "x",
-        ["vi-flip-y"]: flip === "y",
-        ["vi-flip"]: flip === true,
-        ["vi-spin"]: spin === true,
-        ["vi-spin vi-spin-pulse"]: spin === "pulse",
-        ["vi-beat"]: beat,
-        ["vi-fade"]: fade,
-        ["vi-bounce"]: bounce,
+        "vi-rotate-90": rotate === 90,
+        "vi-rotate-180": rotate === 180,
+        "vi-rotate-270": rotate === 270,
+        "vi-flip-x": flip === "x",
+        "vi-flip-y": flip === "y",
+        "vi-flip": flip === true,
+        "vi-spin": spin === true,
+        "vi-spin vi-spin-pulse": spin === "pulse",
+        "vi-beat": beat,
+        "vi-fade": fade,
+        "vi-bounce": bounce,
       })}
       {...props}
     >
