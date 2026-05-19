384a492ed4df24819cfe4055cf1cb87d5447a7e2
Yannick <yannick.franssen@outlook.com>
2026-05-19 12:37:19 +0200

    Move to react plugin instead of swc

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,6 +1,6 @@
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import * as path from "path";
 import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
